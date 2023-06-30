652ba53e05d31e5dbd63b4505ccb8268f1277094
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-29 20:51:04 -0500

    Fixed for XAMPP but not php artisan serve

--- README.md
+++ README.md
@@ -64,3 +64,8 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+## Fixed links
+
+1. Fixed for XAMPP but not php artisan serve - [StackOverflow Reference](https://es.stackoverflow.com/questions/484332/como-resolver-error-en-laravel-the-dump-process-failed-with-exitcode-1-general)
--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -8,21 +8,21 @@ use Spatie\DbDumper\Exceptions\CannotStartDump;
 
 class BackupController extends Controller
 {
-    public function createBackup()
-    {
-        try {
-            $backupPath = 'app/backup.sql';
+  public function createBackup()
+  {
+    try {
+      $backupPath = 'app/backup.sql';
 
-            MySql::create()
-                ->setDbName(env('DB_DATABASE'))
-                ->setUserName(env('DB_USERNAME'))
-                ->setPassword(env('DB_PASSWORD'))
-                ->dumpToFile(storage_path($backupPath));
+      MySql::create()
+        ->setDumpBinaryPath(env('DUMP_BINARY_PATH'))
+        ->setDbName(env('DB_DATABASE'))
+        ->setUserName(env('DB_USERNAME'))
+        ->setPassword(env('DB_PASSWORD'))
+        ->dumpToFile(storage_path($backupPath));
 
-            return redirect()->back()->with('success', '¡Copia de seguridad creada con éxito!');
-        } catch (CannotStartDump $exception) {
-            return redirect()->back()->with('error', 'Error al crear la copia de seguridad: ' . $exception->getMessage());
-        }
+      return redirect()->back()->with('success', '¡Copia de seguridad creada con éxito!');
+    } catch (CannotStartDump $exception) {
+      return redirect()->back()->with('error', 'Error al crear la copia de seguridad: ' . $exception->getMessage());
     }
+  }
 }
-
